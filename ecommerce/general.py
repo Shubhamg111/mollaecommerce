47019e93abad1e8b88138691b48a419fc6f8202c
Shubhamg111 <guptashuvam111@gmail.com>
2024-03-23 14:02:28 +0545

    molla commerce

--- ecommerce/general.py
+++ ecommerce/general.py
@@ -3,7 +3,7 @@ from .models import *
 def global_data(request):
     data= {
         'categoryData': Category.objects.all(),
-        'comData':Settings.objects.all()
+        'comData':Settings.objects.all(),
 
     }
     return data
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,9 +5,30 @@ from .models import *
 
 def home(request):
     data={
-        'bannerData': Banner.objects.filter(is_active=True)
+        'bannerData': Banner.objects.filter(is_active=True),
+        'productData': Product.objects.order_by('-id'),
+
     }
     return render(request,"frontend/pages/index.html",data)
 
 def contact(request):
     return render(request,"frontend/pages/contact.html")
+
+def productview(request,slug):
+    productdata=Product.objects.get(slug=slug)
+    category_id=productdata.category.id
+    relatedproducts = Product.objects.filter(category_id=category_id)
+    data={
+        'productdata':  productdata,
+        'relatedproducts': relatedproducts 
+    }
+    return render(request,'frontend/pages/productview.html',data)
+
+def product_category(request,slug):
+    categoryData = Category.objects.get(slug = slug)
+    productsData = Product.objects.filter(category_id=categoryData.id)
+    data={
+        'productsdata': productsData,
+        
+    }
+    return render(request,'frontend/pages/product_category.html',data)
